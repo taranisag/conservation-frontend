833ae8021802471c9f49966748a170b27d864377
Ibrahem Wattaweda <ibrahemwattaweda@Ibrahem-wattaweda.local>
2025-01-14 13:29:48 +0200

    Set up a new React vite  project with taranis-ui library

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import taranisLogo from "./assets/company-logos/taranis.svg";
+import "./App.css";
+import { Box } from "taranis-ui/lib/components";
+
+function App() {
+  return (
+    <>
+      <Box>
+        <img src={taranisLogo} />
+      </Box>
+      <h1>Taranis Conservation</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+} from "@mui/material";
+import { ThemeLight } from "taranis-ui/lib/theme";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider theme={ThemeLight}>
+      <CssBaseline />
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
