962e423742d8b98fae928af4cdf34421f17b030e
Ibrahem Wattaweda <ibrahemwattaweda@Ibrahem-wattaweda.local>
2025-01-15 15:31:17 +0200

    husky test

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material'
 import { ThemeLight } from 'taranis-ui/lib/theme'
 
