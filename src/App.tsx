5ff02c85206eaa32805624b652771a5b6543f58c
Ibrahem Wattaweda <ibrahemwattaweda@Ibrahem-wattaweda.local>
2025-01-16 14:32:22 +0200

    sass configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Box } from 'taranis-ui/lib/components'
 
 import taranisLogo from './assets/company-logos/taranis.svg'
-import './App.css'
+import './App.scss'
 
 function App(): JSX.Element {
     return (
-        <>
+        <Box className="container">
             <Box>
                 <img src={taranisLogo} alt="Taranis logo" />
             </Box>
             <h1>Taranis Conservation</h1>
-        </>
+        </Box>
     )
 }
 
